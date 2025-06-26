a48e3b67ee71c776ea36fbf4055489b0d08e6101
Mario Ivan Moreno <msmq16@hotmail.com>
2025-06-26 09:30:47 -0600

    Carrga inicial del ejemplo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    bootstrap
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-1q8d3a4b5e6f7c8d9e0f1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Ejemplo</h1>    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+4fXl8Q6d3p5a5e6b5e6f7c8d9e0f1a2b3c4d5e6f7g8h9i0j" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8xW2z5l5e6f7c8d9e0f1a2b3c4d5e6f7g8h9i0j" crossorigin="anonymous"></script>
+</body>
+</html>
